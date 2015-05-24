daa65d5ced7d86cf4186b2e632e7aa8dc01222bc
masterUNG <phrombutr@gmail.com>
2015-05-24 15:11:39 +0700

    finish

--- app/src/main/java/appewtc/masterung/whereewtc23may15/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/whereewtc23may15/MapsActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.whereewtc23may15;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -9,13 +10,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private LatLng btnBangnaLatLng, btnUdomsukLatLng,
-                    ewtcLatLng, userLatLng;
-
+                    ewtcLatLng, userLatLng, bangnaSectionLatLng,
+                    sieiumLatLng, tawonLatLng;
+    private PolylineOptions udomsukPolylineOptions, bangnaPolylineOptions;
+    private double douLat, douLng,
+                    douUdomsukLat = 13.679892, douUdomsukLng = 100.609557,
+                    douBangnaLat = 13.668298, douBangnaLng = 100.604772;
+    private PolygonOptions myPolygonOptions;
+    private boolean statusABoolean = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +40,21 @@ public class MapsActivity extends FragmentActivity {
 
     private void setupLatLng() {
 
-        btnBangnaLatLng = new LatLng(13.668298, 100.604772 );   // นี่คือพิกัดของ BTS บางนา
-        btnUdomsukLatLng = new LatLng(13.679892, 100.609557 );  // Location of BTS Udomsuk
+        btnBangnaLatLng = new LatLng(douBangnaLat, douBangnaLng );   // นี่คือพิกัดของ BTS บางนา
+        btnUdomsukLatLng = new LatLng(douUdomsukLat, douUdomsukLng );  // Location of BTS Udomsuk
         ewtcLatLng = new LatLng(13.667573, 100.621766);         // Location of EWTC
 
         //Receive From Intent to Create userLatlng
-        double douLat = getIntent().getExtras().getDouble("latUser");
-        double douLng = getIntent().getExtras().getDouble("lngUser");
+        douLat = getIntent().getExtras().getDouble("latUser");
+        douLng = getIntent().getExtras().getDouble("lngUser");
         userLatLng = new LatLng(douLat, douLng);
+
+        bangnaSectionLatLng = new LatLng(13.67332216, 100.60691357);    //Location ของแยกบางนา
+        sieiumLatLng = new LatLng(13.66583705, 100.64433575);
+        tawonLatLng = new LatLng(13.66959006, 100.62330723);
+
+
+
     }   // setupLatLng
 
     @Override
@@ -68,7 +84,98 @@ public class MapsActivity extends FragmentActivity {
         //Create Maker
         createMaker();
 
+        //Create Navigator
+        createNavigator();
+
+        //Create Polygon
+        createPolygon();
+
+        //Show Get Event Click Map
+        getEventClickMap();
+
     }   // setUpMap
+
+    private void getEventClickMap() {
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                statusABoolean = !statusABoolean;
+            }
+        });
+
+        if (statusABoolean) {
+            showPolygon();
+        }
+
+    }
+
+    private void createPolygon() {
+
+        myPolygonOptions = new PolygonOptions();
+        myPolygonOptions.add(btnUdomsukLatLng)
+                .add(ewtcLatLng)
+                .add(btnBangnaLatLng)
+                .add(btnUdomsukLatLng)
+                .strokeWidth(5)
+                .strokeColor(Color.MAGENTA)
+                .fillColor(Color.argb(50, 140, 197, 56));
+
+        //showPolygon();
+
+    }   // createPolygon
+
+    private void showPolygon() {
+        mMap.addPolygon(myPolygonOptions);
+    }
+
+    private void createNavigator() {
+
+        //Udomsuk Polyline
+        udomsukPolylineOptions = new PolylineOptions();
+        udomsukPolylineOptions.add(userLatLng)
+                .add(btnUdomsukLatLng)
+                .add(bangnaSectionLatLng)
+                .add(sieiumLatLng)
+                .add(tawonLatLng)
+                .add(ewtcLatLng)
+                .width(10)
+                .color(Color.RED);
+
+        //Bangna Polyline
+        bangnaPolylineOptions = new PolylineOptions();
+        bangnaPolylineOptions.add(userLatLng)
+                .add(btnBangnaLatLng)
+                .add(bangnaSectionLatLng)
+                .add(sieiumLatLng)
+                .add(tawonLatLng)
+                .add(ewtcLatLng)
+                .width(10)
+                .color(Color.BLUE);
+
+        //เลือกเส้นทางที่ใกล้ที่สุด
+        chooseNavigator();
+
+
+    }   // createNavigator
+
+    private void chooseNavigator() {
+
+        double douDistant1 = Math.pow((douLat - douUdomsukLat), 2) + Math.pow((douLng - douUdomsukLng), 2);
+        double douDistant2 = Math.pow((douLat - douBangnaLat), 2) + Math.pow((douLng - douBangnaLng), 2);
+
+        if ((douDistant1 - douDistant2) <= 0) {
+
+            mMap.addPolyline(udomsukPolylineOptions);
+
+        } else {
+
+            mMap.addPolyline(bangnaPolylineOptions);
+
+        }   //if
+
+
+    }   // chooseNavigator
 
     private void createMaker() {
 
